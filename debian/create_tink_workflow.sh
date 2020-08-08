5da36aa2c52180e35012c43b762c2debfaeb145c
Frans van Berckel <fberckel@xs4all.nl>
2020-08-08 09:34:19 +0200

    bullseye.yml: set timeout, create_tink_workflow.sh: add user password & tink workflow events

--- debian/create_tink_workflow.sh
+++ debian/create_tink_workflow.sh
@@ -8,13 +8,16 @@ fi
 export OS_CODENAME=bullseye
 export MAC_ADDRESS=00:1d:21:58:43:5d
 
-read -sp 'Enter new root password ... ' PASS
+read -sp 'Enter new root password: ' ROOTPASSWORD
 printf "\n"
-read -sp 'Enter new password salt ... ' SALT
+read -sp 'Enter new user password: ' USERPASSWORD
+printf "\n"
+read -sp 'Enter new password salt: ' PASSWORDSALT
 printf "\n"
 
 export UUID=$(uuidgen|tr "[:upper:]" "[:lower:]")
-export SALTEDPASSWD=$(printf '%s\n' "$PASS" | mkpasswd --stdin --method=sha-512 --salt "$SALT")
+export CRYPTEDROOTPASSWORD=$(printf '%s\n' "$ROOTPASSWORD" | mkpasswd --stdin --method=sha-512 --salt "$PASSWORDSALT")
+export CRYPTEDUSERPASSWORD=$(printf '%s\n' "$USERPASSWORD" | mkpasswd --stdin --method=sha-512 --salt "$PASSWORDSALT")
 echo "Generate $UUID.json"
 cat ./hardware.json | envsubst  > $UUID.json
 
@@ -23,6 +26,8 @@ docker exec -i deploy_tink-cli_1 tink hardware push < ./$UUID.json
 rm $UUID.json
 
 docker exec -i deploy_tink-cli_1 tink template create --name $OS_CODENAME < ./$OS_CODENAME.yml | tee ./template.txt
-
 TEMPLATE_ID=$(cat ./template.txt | awk '/Created/{printf "%s", $3}' && rm ./template.txt)
+
 docker exec -i deploy_tink-cli_1 tink workflow create --hardware '{"device_1":"'$MAC_ADDRESS'"}' --template "$TEMPLATE_ID" | tee ./workflow.txt
+WORKFLOW_ID=$(cat ./workflow.txt | awk '/Created/{printf "%s", $3}' && rm ./workflow.txt)
+echo "Run 'docker exec -i deploy_tink-cli_1 tink workflow events $WORKFLOW_ID' to view events."
