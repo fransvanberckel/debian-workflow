625299e45263d2ff25f40adea0def4107e1913c2
Frans van Berckel <fberckel@xs4all.nl>
2020-08-24 10:33:00 +0200

    get-package-list.sh: tabs vs spaces

--- debian/00-base/get-package-list.sh
+++ debian/00-base/get-package-list.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 packages=(
-        curl
-        dmidecode
-        efibootmgr
-        file
-        gawk
-        gdisk
-        grml-debootstrap
-        isc-dhcp-client
-        jq
-        lsb-release
-        mdadm
-        nano
-        python3-urllib3
-        wget
+	curl
+	dmidecode
+	efibootmgr
+	file
+	gawk
+	gdisk
+	grml-debootstrap
+	isc-dhcp-client
+	jq
+	lsb-release
+	mdadm
+	nano
+	python3-urllib3
+	wget
 	yq
 )
 echo "${packages[@]}"
